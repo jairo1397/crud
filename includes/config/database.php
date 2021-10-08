d5388f69a2a18df2321f7452aedf20a34e77768c
Jairo Espinoza <50997004+jairo1397@users.noreply.github.com>
2021-10-08 15:48:54 -0500

    config-database

--- includes/config/database.php
+++ includes/config/database.php
@@ -4,7 +4,7 @@ function conectarDB(): mysqli
 {
 
 
-  $db = mysqli_connect('localhost', 'root', '', 'tienda_videojuegos');
+  $db = mysqli_connect('us-cdbr-east-04.cleardb.com', 'bc84c775df6787', '4b0c2b3b', 'heroku_2892a6ec8081b2e');
   if (!$db) {
     echo "<h3>No se ha podido conectar PHP - MySQL, verifique sus datos.</h3><hr><br>";
     exit;
